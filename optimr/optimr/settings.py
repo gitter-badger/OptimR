79715fcff2e2f19cbdbc33c2ddae1fedc9cda1de
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-10-04 11:00:46 +0200

    Fix and implement first view/template

--- optimr/optimr/settings.py
+++ optimr/optimr/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'optimr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- optimr/optimr/views.py
+++ optimr/optimr/views.py
@@ -1,5 +1,6 @@
-from django
+from django.shortcuts import render
+
 
 def home(request):
     context = {'variable': 'hello'}
-    return render
+    return render(request, 'optimr/home.html', context)
